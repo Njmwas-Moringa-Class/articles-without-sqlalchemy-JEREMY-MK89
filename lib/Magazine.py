6e11a0b28239b09651fa21b59afe9d296a3f0263
moses <moses.kingi@moringaschool.com>
2024-02-04 16:49:08 +0300

    classes many to many relations without sql code- debugging using ipdb test

--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,2 +1,25 @@
 class Magazine:
-    pass
+    all_magazines = []
+
+    def __init__(self, name, category):
+        self._name = name
+        self._category = category
+        self._articles = []
+        Magazine.all_magazines.append(self)
+
+    def name(self):
+        return self._name
+
+    def category(self):
+        return self._category
+
+    def contributors(self):
+        return [article.author() for article in self._articles]
+
+    @classmethod
+    def find_by_name(cls, name):
+        return next((magazine for magazine in cls.all_magazines if magazine.name() == name), None)
+
+    @classmethod
+    def article_titles(cls):
+        return [article.title() for magazine in cls.all_magazines for article in magazine._articles]
--- tools/debug.py
+++ tools/debug.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
-import ipdb;
+import sys
+import ipdb
 
+sys.path.append("/root/articles-without-sqlalchemy-JEREMY-MK89")
+
+from lib.Author import Author
+from lib.Article import Article
+from lib.Magazine import Magazine
 
 if __name__ == '__main__':
-#  WRITE YOUR TEST CODE HERE ###
+    # WRITE YOUR TEST CODE HERE
 
+    # Test Author
+    author1 = Author("John Doe")
+    print(author1.name())
+    article1 = author1.add_article(Magazine("Tech Magazine", "Technology"), "Introduction to Python")
+    print(author1.articles())
+    print(author1.magazines())
+    print(author1.topic_areas())
 
+    # Test Magazine
+    magazine1 = Magazine("Tech Magazine", "Technology")
+    print(magazine1.name())
+    print(magazine1.category())
+    print(magazine1.contributors())
+    print(Magazine.find_by_name("Tech Magazine"))
+    print(Magazine.article_titles(magazine1))  
+    print(Magazine.contributing_authors(magazine1)) 
 
+    # Test Article
+    print(article1.title())
+    print(article1.author())
+    print(article1.magazine())
 
-
-
-
-
-
-# DO NOT REMOVE THIS
+    # DO NOT REMOVE THIS
     ipdb.set_trace()
